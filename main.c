bee3c2ea58bdfc6edb034a29d5afbc02d5adf7bc
Herik Lorenção <herik@outlook.com.br>
2018-03-21 14:26:54 -0300

    Adicionando arquivos

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"criar.c"
+#include"editar.c"
+#include"remover.c"
+#include"exibir.c"
+#include"extras.c"
+
+int main(){
+
+    return 0;
+}
